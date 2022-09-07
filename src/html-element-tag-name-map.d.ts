c082dbb035bb0254d8f3180ebc7a3d3cc46b1db1
Muhammad Hamza <muhammadhamza1311@gmail.com>
2022-09-07 21:39:11 +0500

    Initial Commit
    Markdown _editor_ works

---
+++ src/html-element-tag-name-map.d.ts
@@ -0,0 +1,9 @@
+// noinspection JSUnusedGlobalSymbols
+
+import { GuhEditor } from './guh-editor';
+
+declare global {
+    interface HTMLElementTagNameMap {
+        'guh-editor': GuhEditor;
+    }
+}
